3921883ba8ff44bd0d0ae2b8a3a3a9e545ad92aa
Takeshi Oguma <toguma@proudit.jp>
2016-08-29 15:08:57 +0900

    local_run.sh modify

--- local_run.sh
+++ local_run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build -t proudcloud-hp .
-docker run --name proudcloud -v html:/var/www/html -p 80:80 -d proudcloud-hp
+docker run --name proudcloud -v `pwd`/html:/var/www/html -p 80:80 -d proudcloud-hp

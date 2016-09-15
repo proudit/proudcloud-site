3b0d9501dce7330362b9225672a155cc3822bacc
Takeshi Oguma <toguma@proudit.jp>
2016-09-15 13:22:52 +0900

    update func.php for CF

--- html/include/func.php
+++ html/include/func.php
@@ -1,7 +1,8 @@
 <?php
-$ua = $_SERVER["HTTP_USER_AGENT"];
+$ua_m = $_SERVER["HTTP_CLOUDFRONT_IS_MOBILE_VIEWER"];
+$ua_t = $_SERVER["HTTP_CLOUDFRONT_IS_TABLET_VIEWER"];
 $layout_mode = "";
-if(preg_match("/Mobile-Viewer\: true/", $ua) || (preg_match("/Tablet-Viewer\: true/", $ua))) {
+if(preg_match("true/", $ua_m) || (preg_match("true/", $ua_t))) {
 	$layout_mode = "sp";
 }
 else {

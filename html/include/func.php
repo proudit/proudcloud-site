a059ef3e518bb941e2989ac0e9e810da7d1c9b70
Takeshi Oguma <toguma@proudit.jp>
2016-09-15 13:29:30 +0900

    update func.php for CF

--- html/include/func.php
+++ html/include/func.php
@@ -2,7 +2,7 @@
 $ua_m = $_SERVER["HTTP_CLOUDFRONT_IS_MOBILE_VIEWER"];
 $ua_t = $_SERVER["HTTP_CLOUDFRONT_IS_TABLET_VIEWER"];
 $layout_mode = "";
-if(preg_match("true/", $ua_m) || (preg_match("true/", $ua_t))) {
+if(preg_match("true", $ua_m) || (preg_match("true", $ua_t))) {
 	$layout_mode = "sp";
 }
 else {

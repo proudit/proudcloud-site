d8ddb95e748da63489209ce7e1f6fb620068a806
Takeshi Oguma <toguma@proudit.jp>
2016-09-28 09:19:25 +0900

    update staging

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,6 @@ require "serverspec"
 
 set :backend, :docker
 set :docker_url, ENV["DOCKER_HOST"]
-if ENV['DOCKER_IMAGE']
-  set :docker_image, ENV['DOCKER_IMAGE']
-elsif ENV['DOCKER_CONTAINER']
-  set :docker_container, ENV['DOCKER_CONTAINER']
-end
+set :docker_image, "toguma/proudcloud"
 
 Excon.defaults[:ssl_verify_peer] = false

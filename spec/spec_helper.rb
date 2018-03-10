5283a2cea554999dac1f4bfe82a9de4008631bda
toguma <toguma@proudit.jp>
2018-03-10 14:36:53 +0900

    update corporate phone number

--- spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-require "docker"
-require "serverspec"
-
-set :backend, :docker
-set :docker_url, ENV["DOCKER_HOST"]
-set :docker_image, "toguma/proudcloud"
-
-Excon.defaults[:ssl_verify_peer] = false
